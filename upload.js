ada0c5c7be54c207cacedfc02c6b4351a04307b1
Taha Ansari <taha.m.ansari@gmail.com>
2022-11-10 11:05:14 +0530

    excel wip

--- upload.js
+++ upload.js
@@ -4,22 +4,20 @@ let fs = require("fs");
 
 http
   .createServer(function (req, res) {
-    res.write("Server is running.....");
-    res.end();
-
     //Create an instance of the form object
     let form = new formidable.IncomingForm();
-
     //Process the file upload in Node
     form.parse(req, function (error, fields, file) {
       let filepath = file.fileupload.filepath;
-      let newpath = "./uploads/accepted/";
-      newpath += file.fileupload.originalFilename;
-
-      //Copy the uploaded file to a custom folder
-      fs.rename(filepath, newpath, function () {
-        //Send a NodeJS file upload confirmation message
-      });
+      console.log(filepath);
+      // let successPath = "./uploads/accepted/";
+      // successPath += file.fileupload.originalFilename;
+      // //Copy the uploaded file to a custom folder
+      // fs.rename(filepath, successPath, function () {
+      //   //Send a NodeJS file upload confirmation message
+      //   res.write("File Uploaded Successfully..");
+      //   res.end();
+      // });
     });
   })
   .listen(8080);
